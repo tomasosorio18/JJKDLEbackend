11a679ed8b1fb557fc31561359c2537c014a99a4
Tomas Osorio <tomas.osorio@sansano.usm.cl>
2025-09-05 01:05:27 -0400

    fix cors v2

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/images", express.static("public/images"));
 app.use("/voices", express.static("public/voices"));
-const allowedOrigins = ['http://localhost:5173','https://jjk-dle.vercel.app','https://jjk-dle.vercel.app/'];
+const allowedOrigins = ['http://localhost:5173','https://jjk-dle.vercel.app','https://jjk-dle.vercel.app/', 'https://jjkdle-phuv0yveg-tomasosorio18s-projects.vercel.app', 'https://jjkdle-phuv0yveg-tomasosorio18s-projects.vercel.app/'];
   app.use(cors({
     origin: allowedOrigins,
     credentials: true
